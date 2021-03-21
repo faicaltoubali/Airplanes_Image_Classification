fd25458b8739da1bbe2c116f52db07a251343c04
Faical <faical.toubalihadaoui@gmail.com>
2021-03-21 16:05:47 +0100

    Eurofighter Typhoon

--- Dataset/Test/Military/rename.py
+++ Dataset/Test/Military/rename.py
@@ -3,10 +3,10 @@ import os
 # Function to rename multiple files 
 def main(): 
   
-    for count, filename in enumerate(os.listdir("A400M")): 
-        dst ="Military-A400M-" + str(count+1) + ".jpg"
-        src ='A400M/'+ filename 
-        dst ='A400M/'+ dst 
+    for count, filename in enumerate(os.listdir("Eurofighter_Typhoon")): 
+        dst ="Military-Eurofighter_Typhoon-" + str(count+1) + ".jpg"
+        src ='Eurofighter_Typhoon/'+ filename 
+        dst ='Eurofighter_Typhoon/'+ dst 
           
         # rename() function will 
         # rename all the files 
