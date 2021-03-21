d4b65f7d1f144b15b30332683160a98588f5c11b
Faical <faical.toubalihadaoui@gmail.com>
2021-03-21 15:46:54 +0100

    Soukhoi Su-57

--- Dataset/Validation/Military/rename.py
+++ Dataset/Validation/Military/rename.py
@@ -3,10 +3,10 @@ import os
 # Function to rename multiple files 
 def main(): 
   
-    for count, filename in enumerate(os.listdir("Rafale")): 
-        dst ="Military-Rafale-" + str(count+1) + ".jpg"
-        src ='Rafale/'+ filename 
-        dst ='Rafale/'+ dst 
+    for count, filename in enumerate(os.listdir("Su-57")): 
+        dst ="Military-Su57-" + str(count+1) + ".jpg"
+        src ='Su-57/'+ filename 
+        dst ='Su-57/'+ dst 
           
         # rename() function will 
         # rename all the files 
